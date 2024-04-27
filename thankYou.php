58a15c056493451656de7527fa03e9286066834b
stotoki <totoki.sayoko@gmail.com>
2024-04-27 21:12:11 +0900

    DBed thankyou and bookmark

--- thankYou.php
+++ thankYou.php
@@ -10,35 +10,67 @@ var_dump($_POST);
 $thankYou = $_POST['thankYou'];
 $bookmark = $_POST['bookmark'];
 // recipe_idの取得が必要
+    $str_recipe_id = $_POST['recipe_id'];
+    // str型の$str_recipe_idをint型に変換
+    $recipe_id = intval($str_recipe_id);
 
 // $dataArrを作成する
-
-
-
-
+$dataArr = [
+    'user_id' => $login_user['id'],
+    'recipe_id' => $recipe_id,
+    'thankyou'  => $thankYou,
+    'bookmark'  => $bookmark
+];
 
 // 「ごちそうさま」をDBに保存する
-$result = FALSE;
+if ($dataArr['thankyou']) {
+    $result = FALSE;
 
-$sql = "INSERT INTO thankyous
+    $sql = "INSERT INTO thankyous
         (recipe_id, user_id)
         VALUE
         (?, ?)";
 
-try {
-    $stmt = connect()->prepare($sql);
-    $stmt->bindValue(1, $dataArr['login_user']);
-    $stmt->bindValue(2, $dataArr['login_user']);
-    $result = $stmt->execute();
-} catch (\Exception $e) {
-    echo $e->getMessage();
+    try {
+        $stmt = connect()->prepare($sql);
+        $stmt->bindValue(1, $dataArr['user_id']);
+        $stmt->bindValue(2, $dataArr['recipe_id']);
+        $result = $stmt->execute();
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+
+    if ($result === FALSE) {
+        echo 'データベースへの保存が失敗しました！';
+    } else {
+        echo 'データベースに保存しました！';
+    }
 }
 
-if ($result === FALSE) {
-    echo 'データベースへの保存が失敗しました！';
-} else {
-    echo 'データベースに保存しました！';
+// 「ブックマーク」をDBに保存する
+if ($dataArr['bookmark']) {
+    $result = FALSE;
+
+    $sql = "INSERT INTO bookmarks
+        (recipe_id, user_id)
+        VALUE
+        (?, ?)";
+
+    try {
+        $stmt = connect()->prepare($sql);
+        $stmt->bindValue(1, $dataArr['user_id']);
+        $stmt->bindValue(2, $dataArr['recipe_id']);
+        $result = $stmt->execute();
+    } catch (\Exception $e) {
+        echo $e->getMessage();
+    }
+
+    if ($result === FALSE) {
+        echo 'データベースへの保存が失敗しました！';
+    } else {
+        echo 'データベースに保存しました！';
+    }
 }
 
-// header("Location: general_top.php");
+header("Location: general_top.php");
 ?>
